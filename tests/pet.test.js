763787f2702b4ddd77a8fbcf57decd5490d13df1
nic-bes <nic_bes@icloud.com>
2020-05-21 17:25:28 +0100

    new properties added to growUp method (fitness & hunger) with corresponding unit tests

--- tests/pet.test.js
+++ tests/pet.test.js
@@ -14,20 +14,21 @@ describe('constructor', () => {
 
         expect(pet.age).toEqual(0);
     });
-    describe('growUp', () => {
-        it('increments the age by 1', () => {
-            const pet = new Pet('Fido');
-
-            pet.growUp();
-
-            expect(pet.age).toEqual(1);
-        });
-    });
 });
 describe('growUp', () => {
     it('increments the age by 1', () => {
         const pet = new Pet('Fido');
         pet.growUp();
         expect(pet.age).toEqual(1);
+    });
+    it('increments the hunger by 5', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.hunger).toEqual(5);
+    });
+    it('decrements the fitness by 3', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.fitness).toEqual(7);
     });
 });
