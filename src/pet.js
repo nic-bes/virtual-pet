6fa85c45a734690705980b5a2d114d659ee889d3
nic-bes <nic_bes@icloud.com>
2020-05-21 18:11:25 +0100

    first refactor MAXIMUM_FITNESS const added to pet.js

--- src/pet.js
+++ src/pet.js
@@ -1,8 +1,10 @@
+const MAXIMUM_FITNESS = 10;
+
 function Pet(name) {
     this.name = name;
     this.age = 0;
     this.hunger = 0;
-    this.fitness = 10;
+    this.fitness = MAXIMUM_FITNESS;
 }
 Pet.prototype.growUp = function () {
     this.age += 1;
@@ -10,10 +12,10 @@ Pet.prototype.growUp = function () {
     this.fitness -= 3;
 };
 Pet.prototype.walk = function () {
-    if ((this.fitness + 4) <= 10) {
+    if ((this.fitness + 4) <= MAXIMUM_FITNESS) {
         this.fitness += 4;
     } else {
-        this.fitness = 10;
+        this.fitness = MAXIMUM_FITNESS;
     }
 };
 
