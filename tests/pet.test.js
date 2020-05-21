3a228bcbe59cbc750d99c37d78784ef53274abaf
nic-bes <nic_bes@icloud.com>
2020-05-21 15:48:49 +0100

    pet.name set and npm test passed

--- tests/pet.test.js
+++ tests/pet.test.js
@@ -4,4 +4,11 @@ describe('constructor', () => {
     it('returns an object', () => {
         expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
+});
+
+describe('constructor', () => {
+    it('sets the name property', () => {
+        const pet = new Pet('Fido');
+        expect(pet.name).toEqual('Fido');
+    });
 });       
